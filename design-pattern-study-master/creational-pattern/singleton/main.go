4f87445492711e46585b928a54b0fdbf2bdcb83f
binggu <441282413@qq.com>
2025-05-18 20:09:53 +0800

    fix: 设计模式模块，包管理修正

--- design-pattern-study-master/creational-pattern/singleton/main.go
+++ design-pattern-study-master/creational-pattern/singleton/main.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-    "fmt"
-    "github.com/BingguWang/design-pattern-study/creational-pattern/singleton/model"
-    "sync"
+	"design-pattern-study-master/creational-pattern/singleton/model"
+	"fmt"
+
+	"sync"
 )
 
-//懒加载，用sync.Once实现并发安全的单例
+// 懒加载，用sync.Once实现并发安全的单例
 func main() {
-    //testSingleton()
-    testSingletonParallel()
+	//testSingleton()
+	testSingletonParallel()
 }
 
-//单线程判断是否是单例
+// 单线程判断是否是单例
 func testSingleton() {
-    //获取单例对象
-    apple1 := model.GetSingletonInstance()
-    apple2 := model.GetSingletonInstance()
-    fmt.Printf("是否是单例:%v\n", apple1 == apple2)
+	//获取单例对象
+	apple1 := model.GetSingletonInstance()
+	apple2 := model.GetSingletonInstance()
+	fmt.Printf("是否是单例:%v\n", apple1 == apple2)
 
 }
 
-//多线程下判断是否还是单例
+// 多线程下判断是否还是单例
 const count = 100
 
 func testSingletonParallel() {
-    var wait sync.WaitGroup
-    wait.Add(count)
-    appleList := make([]*model.Instance, count)
-    for i := 0; i < count; i++ {
-        go func(index int) {
-            //fmt.Printf("%T\n",appleList)
-            appleList[index] = model.GetSingletonInstance()
-            wait.Done()
-        }(i)
-    }
-    wait.Wait()
-    for i := 1; i < count; i++ {
-        if appleList[i-1] != appleList[i] {
-            fmt.Println("不满足单例")
-            return
-        }
-    }
-    fmt.Println("满足单例")
+	var wait sync.WaitGroup
+	wait.Add(count)
+	appleList := make([]*model.Instance, count)
+	for i := 0; i < count; i++ {
+		go func(index int) {
+			//fmt.Printf("%T\n",appleList)
+			appleList[index] = model.GetSingletonInstance()
+			wait.Done()
+		}(i)
+	}
+	wait.Wait()
+	for i := 1; i < count; i++ {
+		if appleList[i-1] != appleList[i] {
+			fmt.Println("不满足单例")
+			return
+		}
+	}
+	fmt.Println("满足单例")
 }
--- design-pattern-study-master/structural-pattern/wrapper/main.go
+++ design-pattern-study-master/structural-pattern/wrapper/main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "github.com/BingguWang/design-pattern-study/structural-pattern/wrapper/model"
+	"design-pattern-study-master/structural-pattern/wrapper/model"
+	"fmt"
 )
 
-/**
-  装饰器模式：
-      wrapper
-        将对象放入  包含行为的特殊封装对象中   来为  原对象  绑定新的行为。而不用为此去新建一个子类
+/*
+*
 
-    可以防止子类数量爆炸
+	装饰器模式：
+	    wrapper
+	      将对象放入  包含行为的特殊封装对象中   来为  原对象  绑定新的行为。而不用为此去新建一个子类
 
-    当你希望在运行时为对象新增额外的行为， 可以使用装饰模式。而不需要去修改对象
+	  可以防止子类数量爆炸
 
-    实现：
-        基础行为放到基类，新的行为放到装饰器里
-        目标对象和装饰器遵循同一接口，
+	  当你希望在运行时为对象新增额外的行为， 可以使用装饰模式。而不需要去修改对象
+
+	  实现：
+	      基础行为放到基类，新的行为放到装饰器里
+	      目标对象和装饰器遵循同一接口，
 
 其实核心就是聚合！！！的思想，用聚合代替了继承，更加灵活，继承有严重的耦合性，聚合没有
 
-    很多代码里都可以看到，这个是很常见的模式
-
+	很多代码里都可以看到，这个是很常见的模式
 */
 func main() {
-    mobile := &model.Mobile{}
+	mobile := &model.Mobile{}
 
-    mobilewithgame := &model.MobileWithGame{Mobile: mobile}
-    mobilewithwechat := &model.MobileWithWeChat{Mobile: mobilewithgame}
+	mobilewithgame := &model.MobileWithGame{Mobile: mobile}
+	mobilewithwechat := &model.MobileWithWeChat{Mobile: mobilewithgame}
 
-    fmt.Println("功能有：")
-    mobilewithwechat.Function()
+	fmt.Println("功能有：")
+	mobilewithwechat.Function()
 }
